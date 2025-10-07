61d1c72e6e9d08548a479bd7e5bede66a66d7260
Renan Ribeiro Jungles de Lima <renan.lima@i4pro.com.br>
2025-10-07 10:04:27 -0300

    -- Criação das bats de inicialização do servidor
     Arquivos modificados:
    	modified:   backend/__pycache__/database.cpython-313.pyc
    	modified:   backend/__pycache__/main.cpython-313.pyc
    	modified:   backend/__pycache__/models.cpython-313.pyc
    	modified:   backend/__pycache__/schemas.cpython-313.pyc
    	new file:   backend/start_server.bat
    	new file:   backend/start_server.ps1

---
+++ backend/start_server.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\ProjetosGIT\Hackathon\Projeto-Hackathon-1\backend"
+call "venv\Scripts\activate.bat"
+python main.py
+pause
---
+++ backend/start_server.ps1
@@ -0,0 +1,4 @@
+# Script PowerShell para ativar ambiente virtual e executar servidor
+Set-Location "C:\ProjetosGIT\Hackathon\Projeto-Hackathon-1\backend"
+& ".\venv\Scripts\Activate.ps1"
+python main.py
